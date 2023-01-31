0d1947cebf8248898bf797a1b61e9c7d77c6a904
Olulanke-Mainasara <95680556+Olulanke-Mainasara@users.noreply.github.com>
2023-01-31 14:55:23 +0100

    Fixed the animation bug and added a new favicon :)

--- app/Components/News.jsx
+++ app/Components/News.jsx
@@ -1,22 +1,24 @@
+import React from 'react';
 import Image from 'next/image'
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 
-function News({ news, controls }) {
+function News({ width, news, controls }) {
   return (
     <div className='h-full flex'>
           {
             news.map(newsItem => {
               return (
                 <motion.div className={`w-screen h-full`} animate={controls} key={newsItem.id}>
-                  <div className="carousel-item w-[90%] h-full overflow-hidden rounded-2xl mx-auto bg-black list-none flex items-center justify-center">
-                      <div className='relative basis-3/4 w-full h-full'>
-                          <Image src={newsItem.imgsrc} fill priority alt="" />
+                  <div className="w-[90%] h-full overflow-hidden rounded-2xl mx-auto bg-black list-none flex items-center justify-center allM:flex-col">
+                      <div className='relative basis-3/4 md:basis-1/2 allEM:basis-2/5 allT:basis-2/5 w-full h-full'>
+                          <Image src={newsItem.imgsrc} fill priority alt="News Image" />
                       </div>
 
-                      <div className="text-white flex flex-col items-center justify-center text-center gap-6 basis-2/5 px-6">
-                          <h5 className="text-xl">{newsItem.sn}</h5>
-                          <p className="text-5xl leading-[60px]">{newsItem.text}<br /><Link className="text-xl border rounded-2xl p-4 hover:text-gray-800 hover:bg-white transition-colors duration-500" href="#">Read more</Link></p>
+                      <div className="text-white flex flex-col items-center justify-center gap-6 basis-2/5 md:basis-1/2 allEM:basis-3/5 allT:basis-3/5 allM:justify-evenly allM:gap-0 px-6">
+                          <h5 className="text-xl allT:text-lg">{newsItem.sn}</h5>
+                          <p className="text-5xl leading-[60px] allEM:text-3xl allEM:leading-[45px] allT:text-2xl">{newsItem.text}</p>
+                          <Link className="text-xl border rounded-2xl p-4 allEM:p-3 allEM:text-lg allT:p-3 allT:text-base hover:text-gray-800 hover:bg-white transition-colors duration-500" href="#">Read more</Link>
                       </div>
                   </div>
                 </motion.div>
--- app/Components/NewsCarousel.jsx
+++ app/Components/NewsCarousel.jsx
@@ -9,17 +9,24 @@ import { motion, useAnimation } from 'framer-motion';
 import { news } from './Arrays';
 import News from './News';
 
-let innerWidth = window.innerWidth
 
 function NewsCarousel() {
-  const [currentSlide, setCurrentSlide] = useState(-1);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [width, setWidth] = useState(window.innerWidth);
+
   const controls = useAnimation();
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    window.addEventListener("resize", () => setWidth(window.innerWidth));
+    return () => window.removeEventListener("resize", () => setWidth(window.innerWidth));
+  }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentSlide(prevSlide => {
-        if (prevSlide === news.length - 2) {
-          return -1;
+        if (prevSlide === news.length - 1) {
+          return 0;
         }
         return prevSlide + 1;
       });
@@ -29,15 +36,15 @@ function NewsCarousel() {
 
   useEffect(() => {
     controls.start({
-      x: -currentSlide * innerWidth,
+      x: -currentSlide * width,
       transition: { duration: 2 },
     });
   }, [currentSlide]);
 
   function nextNews() {
     setCurrentSlide(prevSlide => {
-      if (prevSlide === news.length - 2) {
-        return -1;
+      if (prevSlide === news.length - 1) {
+        return 0;
       }
       return prevSlide + 1;
     });
@@ -46,21 +53,21 @@ function NewsCarousel() {
   function prevNews() {
     setCurrentSlide(prevSlide => {
       if (prevSlide === -1) {
-        return news.length - 2;
+        return news.length - 1;
       }
       return prevSlide - 1;
     });
   }
 
   return (
-    <section className="w-full overflow-hidden flex flex-col gap-10 items-center justify-center my-20 allM:hidden lg:hidden">
-      <h1 className="dark:text-white text-8xl">
+    <section className="w-full overflow-hidden flex flex-col gap-10 items-center justify-center my-28 text-center">
+      <h1 className="dark:text-white text-8xl md:text-7xl allEM:text-5xl allT:text-3xl">
         Today's Featured Articles
       </h1>
 
-      <motion.div className="relative w-screen h-[500px] 2xl:h-[650px] flex items-center justify-center overflow-x-hidden">
-        <Link title="Previous page" className="w-12 h-12 flex items-center justify-center z-10 bg-none absolute border-none rounded-full text-xl transition-colors hover:bg-black hover:text-white top-1/2 -translate-y-1/2 left-2 duration-300 allEM:shadow-none allT:shadow-none text-black" onClick={ prevNews } href="#"><FontAwesomeIcon icon={ faChevronLeft } /></Link>
-        <Link title="Next page" className="w-12 h-12 flex items-center justify-center z-10 bg-none absolute border-none rounded-full text-xl transition-colors hover:bg-black hover:text-white top-1/2 -translate-y-1/2 right-2 duration-300 allEM:shadow-none allT:shadow-none text-black" onClick={ nextNews } href="#"><FontAwesomeIcon icon={ faChevronRight } /></Link>
+      <motion.div className="relative w-screen h-[500px] 2xl:h-[650px] md:h-[800px] allT:h-[420px] flex items-center overflow-x-hidden">
+        <Link title="Previous page" className="w-12 allEM:w-6 allT:w-4 h-12 flex items-center justify-center z-10 bg-none absolute border-none rounded-full text-xl transition-colors hover:bg-black hover:text-white top-1/2 -translate-y-1/2 left-2 allIL:left-0 duration-300 allIL:hover:bg-transparent allIL:hover:text-black allEM:shadow-none allT:shadow-none text-black" onClick={ prevNews } href="#"><FontAwesomeIcon icon={ faChevronLeft } /></Link>
+        <Link title="Next page" className="w-12 allEM:w-6 allT:w-4 h-12 flex items-center justify-center z-10 bg-none absolute border-none rounded-full text-xl transition-colors hover:bg-black hover:text-white top-1/2 -translate-y-1/2 right-2 allIL:right-0 duration-300 allIL:hover:bg-transparent allIL:hover:text-black allEM:shadow-none allT:shadow-none text-black" onClick={ nextNews } href="#"><FontAwesomeIcon icon={ faChevronRight } /></Link>
 
         <News news={news} controls={controls}/>
       </motion.div>
