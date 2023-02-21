c27cb383b249a44c313f3e1c342185342e1ea6e1
Olulanke-Mainasara <95680556+Olulanke-Mainasara@users.noreply.github.com>
2023-02-21 09:15:25 +0100

    Formatted the code :)

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,22 @@
-import './globals.css'
-import Nav from './Components/sections/Nav'
-import Footer from './Components/sections/Footer'
+import "./globals.css";
+import Nav from "./Components/sections/Nav";
+import Footer from "./Components/sections/Footer";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className='dark'>
+    <html lang="en" className="dark">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className='dark:bg-black'>
+      <body className="dark:bg-black">
         <Nav />
-        <div className='alphaContainer allIL:h-auto'>
+        <div className="alphaContainer allIL:h-auto">
           {children}
           <Footer />
-        </div>  
+        </div>
       </body>
     </html>
-  )
+  );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,10 @@
-import Hero from './Components/sections/Hero'
-import SectionOne from './Components/sections/SectionOne'
-import NewsCarousel from './Components/features/News-Carousel/NewsCarousel'
-import SectionThree from './Components/sections/SectionThree'
-import CategoriesCarousel from './Components/features/Categories-Carousel/CategoriesCarousel'
-import SectionFive from './Components/sections/SectionFive'
-import ReviewsCarousel from './Components/features/Reviews-Carousel/ReviewsCarousel'
-
+import Hero from "./Components/sections/Hero";
+import SectionOne from "./Components/sections/SectionOne";
+import NewsCarousel from "./Components/features/News-Carousel/NewsCarousel";
+import SectionThree from "./Components/sections/SectionThree";
+import CategoriesCarousel from "./Components/features/Categories-Carousel/CategoriesCarousel";
+import SectionFive from "./Components/sections/SectionFive";
+import ReviewsCarousel from "./Components/features/Reviews-Carousel/ReviewsCarousel";
 
 export default function Home() {
   return (
@@ -20,5 +19,5 @@ export default function Home() {
         <ReviewsCarousel />
       </main>
     </>
-  )
+  );
 }
